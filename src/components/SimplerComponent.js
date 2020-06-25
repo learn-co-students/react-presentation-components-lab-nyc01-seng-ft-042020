a540a15872c54800625978296b947b8a40172772
blobbyblobfish <aywang6@gmail.com>
2020-06-24 22:45:57 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
 // Code SimplerComponent Here
+
+import { ProgressPlugin } from "webpack";
+
+export default function SimplerComponent() {
+
+    const handleClick = () => {
+        console.log("hi!")
+    }
+
+    return(<div onClick={handleClick}>I am just happy</div>)
+}
